c0b61017314c3581cfdbdf93d5f0e57b3e106626
Mbrasebin <mickael.brasebin@gmail.com>
2020-10-21 10:12:50 +0200

    Bat update

--- bundle/run_simplu3d.bat
+++ bundle/run_simplu3d.bat
@@ -4,4 +4,4 @@ cd /D "%~dp0"
 
 
 
-java -Xms1024m -Xmx1024m -XX:PermSize=256m -Djava.library.path=./lib/native_libraries/windows-amd64 -XX:MaxPermSize=256m -jar simplu3d-grenoble-1.0-SNAPSHOT-shaded.jar %*
+java -Xms8G -Xmx8G -XX:PermSize=256m -Djava.library.path=./lib/native_libraries/windows-amd64 -XX:MaxPermSize=256m -jar simplu3d-grenoble-1.0-SNAPSHOT-shaded.jar %*
